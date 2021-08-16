3c7c42d086dc3a60f820e5d7b36afea501012741
kuimgame77 <76770335+kuimgame77@users.noreply.github.com>
2021-08-16 10:18:43 +0900

    Update PauseCommand.ts

--- src/commands/PauseCommand.ts
+++ src/commands/PauseCommand.ts
@@ -5,7 +5,8 @@ import { createEmbed } from "../utils/createEmbed";
 import { IMessage } from "../../typings";
 
 @DefineCommand({
-    description: "Pause the music player",
+    aliases: ["dlfwlwjdwl", "일시중지"],
+    description: "노래를 일시중지합니다",
     name: "pause",
     usage: "{prefix}pause"
 })
@@ -17,7 +18,7 @@ export class PauseCommand extends BaseCommand {
         if (message.guild?.queue?.playing) {
             message.guild.queue.playing = false;
             message.guild.queue.connection?.dispatcher.pause();
-            return message.channel.send(createEmbed("info", "⏸ **|** The music player has been paused"));
+            return message.channel.send(createEmbed("info", "⏸ **|** 음악 플레이어가 일시 중지되었습니다."));
         }
         message.channel.send(createEmbed("error", "The music player is already paused"))
             .catch(e => this.client.logger.error("PAUSE_CMD_ERR:", e));
