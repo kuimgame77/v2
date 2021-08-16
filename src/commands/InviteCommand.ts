cf62e0dbf4ea595539282245e40dbe3b0251326a
kuimgame77 <76770335+kuimgame77@users.noreply.github.com>
2021-08-16 10:15:51 +0900

    Update InviteCommand.ts

--- src/commands/InviteCommand.ts
+++ src/commands/InviteCommand.ts
@@ -5,7 +5,8 @@ import { disableInviteCmd } from "../config";
 import { IMessage } from "../../typings";
 
 @DefineCommand({
-    description: "Send the bot's invite link",
+    aliases: ["cheo", "초대"],
+    description: "초대 링크 보내기",
     disable: disableInviteCmd,
     name: "invite",
     usage: "{prefix}invite"
@@ -14,7 +15,7 @@ export class InviteCommand extends BaseCommand {
     public async execute(message: IMessage): Promise<void> {
         message.channel.send(
             createEmbed("info")
-                .addField(`${this.client.user!.tag} - Invite Link`, `**[Click here to invite this bot](${await this.client.generateInvite({ permissions: 53857345 })})**`)
+                .addField(`${this.client.user!.tag} - 초대 링크`, `**[랑크](${await this.client.generateInvite({ permissions: 53857345 })})**`)
         ).catch(e => this.client.logger.error("PLAY_CMD_ERR:", e));
     }
 }
